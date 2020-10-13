861675be0686a1c557d9159e055d056cf83d2083
Thobeats <41216440+Thobeats@users.noreply.github.com>
2020-10-13 16:17:07 +0100

    update project

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,7 +1,25 @@
 //using selectors inside the element
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function(question){
+    const btn = question.querySelector(".question-btn");
+    btn.addEventListener("click", function(){
+        questions.forEach(function(item){
+            if(item !== question){
+                item.classList.remove("show-text");
+            }
+        });
+        question.classList.toggle("show-text");
+          
+    });
+});
+
+
+
 // traversing the dom
 
-const btns = document.querySelectorAll(".question-btn");
+/*const btns = document.querySelectorAll(".question-btn");
 
 btns.forEach(function(btn){
     btn.addEventListener("click", function(e){
@@ -9,4 +27,4 @@ btns.forEach(function(btn){
 
         question.classList.toggle("show-text"); 
     });
-});
+}); */
