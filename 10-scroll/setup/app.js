6151f0494c63d65cf4fb08a20c4c74c5f02df884
Thobeats <41216440+Thobeats@users.noreply.github.com>
2020-10-15 12:30:27 +0100

    Scroll

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -4,10 +4,74 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+const linksContainer = document.querySelector(".links-container"); 
 
+navToggle.addEventListener("click", function(){
+  //  linksContainer.classList.toggle("show-links");
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+  
+  if(containerHeight === 0){
+    linksContainer.style.height = `${linksHeight}px`;
+  }else{
+    linksContainer.style.height = 0;
+  }
+});
 // ********** fixed navbar ************
+const navbar = document.getElementById("nav");
+const toplink = document.querySelector(".top-link");
+window.addEventListener('scroll', function(){
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
 
+    if(scrollHeight > navHeight){
+        navbar.classList.add("fixed-nav");
+    }else {
+        navbar.classList.remove("fixed-nav");
+    }
+
+    if(scrollHeight > 500){     
+        toplink.classList.add("show-link");
+    }else {
+        toplink.classList.remove("show-link");
+    }
+});
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(function(link){
+    link.addEventListener("click", function(e){
+        e.preventDefault();
+
+        //navigate to specific spot
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+
+        //calculate the heights
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains("fixed-nav"); 
+        let position = element.offsetTop - navHeight;
+        //console.log(position);
+
+        if(!fixedNav){
+            position = position - navHeight;
+        }
+
+        if(navHeight > 82){
+            position = position + containerHeight;
+        }
+        window.scrollTo({
+            left:0, 
+            top:position,
+        });
+
+        linksContainer.style.height = 0;
+    });
+});
